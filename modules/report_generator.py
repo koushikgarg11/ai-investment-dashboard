351eacdbc8f8bd71fb753ab3c16b62dac87f08bf
koushikgarg11 <145890848+koushikgarg11@users.noreply.github.com>
2026-03-11 13:22:23 +0530

    Update report_generator.py

--- modules/report_generator.py
+++ modules/report_generator.py
@@ -1,37 +1,32 @@
 from fpdf import FPDF
 
 
-def generate_pdf_report(report_data):
+def generate_report(data):
 
     pdf = FPDF()
     pdf.add_page()
 
-    pdf.set_font("Arial", "B", 16)
-    pdf.cell(0, 10, "Global Investment Intelligence Report", ln=True)
+    pdf.set_font("Arial","B",16)
+    pdf.cell(0,10,"Global Investment Report",ln=True)
 
     pdf.ln(10)
 
-    pdf.set_font("Arial", size=12)
+    pdf.set_font("Arial","",12)
 
-    pdf.cell(0, 10, f"Country: {report_data.get('Country','N/A')}", ln=True)
-    pdf.cell(0, 10, f"GDP: {report_data.get('GDP','N/A')}", ln=True)
-    pdf.cell(0, 10, f"Inflation: {report_data.get('Inflation','N/A')}", ln=True)
-    pdf.cell(0, 10, f"Population: {report_data.get('Population','N/A')}", ln=True)
-    pdf.cell(0, 10, f"Investment Score: {report_data.get('Investment Score','N/A')}", ln=True)
+    for k,v in data.items():
 
-    pdf.ln(10)
+        if isinstance(v,list):
 
-    pdf.set_font("Arial", "B", 14)
-    pdf.cell(0, 10, "Recommended Industries", ln=True)
+            pdf.cell(0,10,f"{k}:",ln=True)
 
-    pdf.set_font("Arial", size=12)
+            for i in v:
+                pdf.cell(0,10,f"- {i}",ln=True)
 
-    industries = report_data.get("Industries", [])
+        else:
 
-    for ind in industries:
-        pdf.cell(0, 10, f"- {ind}", ln=True)
+            pdf.cell(0,10,f"{k}: {v}",ln=True)
 
-    file_name = "investment_report.pdf"
-    pdf.output(file_name)
+    file="investment_report.pdf"
+    pdf.output(file)
 
-    return file_name
+    return file
