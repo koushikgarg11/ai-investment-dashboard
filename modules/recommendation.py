4c31dd7893580a4c0f9f9ab1440f872f7bcc0fa8
koushikgarg11 <145890848+koushikgarg11@users.noreply.github.com>
2026-03-11 13:06:01 +0530

    Update recommendation.py

--- modules/recommendation.py
+++ modules/recommendation.py
@@ -1,4 +1,8 @@
 from modules.kpi_engine import top_industries
 
+
 def get_recommendations(country):
-    return top_industries(country)
+
+    industries = top_industries(country)
+
+    return industries
