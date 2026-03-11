6ad18a7fb99391f2f616abf62ef82b85566e3ee0
koushikgarg11 <145890848+koushikgarg11@users.noreply.github.com>
2026-03-11 12:54:59 +0530

    Update data_loader.py

--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,12 +1,25 @@
 import pandas as pd
+import requests
 import streamlit as st
 
-@st.cache_data(ttl=60*60*24*180)  # cache for 6 months
-def load_macro_data(file_path="datasets/macro_data.csv"):
-    """
-    Load macroeconomic data for all countries.
-    Columns example: Country,GDP,Inflation,FDI,Unemployment
-    """
-    df = pd.read_csv(file_path)
+@st.cache_data
+def load_macro_data():
+
+    url = "https://api.worldbank.org/v2/country/all/indicator/NY.GDP.MKTP.CD?format=json&per_page=20000"
+
+    response = requests.get(url)
+    data = response.json()[1]
+
+    records = []
+
+    for item in data:
+        if item["value"] is not None:
+            records.append({
+                "Country": item["country"]["value"],
+                "Year": item["date"],
+                "GDP": item["value"]
+            })
+
+    df = pd.DataFrame(records)
+
     return df
-  
